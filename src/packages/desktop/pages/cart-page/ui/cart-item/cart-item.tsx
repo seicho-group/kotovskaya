230a9a043af3c4c9306c0f282962cea7bfee92f4
Konstaphy <Konstaphy@gmail.com>
2024-05-19 00:09:18 +0500

    fix: перевез корзину на одну компоненту

--- src/packages/desktop/pages/cart-page/ui/cart-item/cart-item.tsx
+++ src/packages/desktop/pages/cart-page/ui/cart-item/cart-item.tsx
@@ -17,10 +17,9 @@ export function CartItem({ product }: TProps) {
   const { cart, deleteProduct, incrementById, decrementById } = useCartStore()
 
   return (
-    <div>
-      <div className="cartitem__area">
-        <div className="center">
-          {/* <img
+    <div className="cartitem__area">
+      <div className="center">
+        {/* <img
             onError={(e) => {
               // @ts-ignore
               e.target.src = pic
@@ -29,48 +28,47 @@ export function CartItem({ product }: TProps) {
             src={getImageUrl(product.id)}
             alt=""
           /> */}
-          <Image imageLink={product.imageLink} />
-        </div>
-        <div className="cartitem__name">
-          {" "}
-          <Link to={`/product/${product.id}`}>
-            <p>{product.name}</p>
-          </Link>
-          <div>{product.price / 100 + "₽"}</div>
-        </div>
-        <div className="cartitem__quantity">
-          <div className="cartitem__quantity__buttons">
-            <button
-              className="cartitem__quantity__button"
-              onClick={() => {
-                if (cart[product.id].accumulator === 1) {
-                  deleteProduct(product.id)
-                }
-                decrementById(product.id)
-              }}
-            >
-              -
-            </button>
-            <div className="cartitem__quantity">
-              {cart[product.id]?.accumulator}
-            </div>
-            <button
-              className="cartitem__quantity__button"
-              onClick={() => {
-                incrementById(product.id)
-              }}
-            >
-              +
-            </button>
+        <Image imageLink={product.imageLink} />
+      </div>
+      <div className="cartitem__name">
+        {" "}
+        <Link to={`/product/${product.id}`}>
+          <p>{product.name}</p>
+        </Link>
+        <div>{product.price / 100 + "₽"}</div>
+      </div>
+      <div className="cartitem__quantity">
+        <div className="cartitem__quantity__buttons">
+          <button
+            className="cartitem__quantity__button"
+            onClick={() => {
+              if (cart[product.id].accumulator === 1) {
+                deleteProduct(product.id)
+              }
+              decrementById(product.id)
+            }}
+          >
+            -
+          </button>
+          <div className="cartitem__quantity">
+            {cart[product.id]?.accumulator}
           </div>
+          <button
+            className="cartitem__quantity__button"
+            onClick={() => {
+              incrementById(product.id)
+            }}
+          >
+            +
+          </button>
         </div>
-        <div
-          className="cartitem__delete__area"
-          onClick={() => deleteProduct(product.id)}
-          style={{ cursor: "pointer" }}
-        >
-          <img className="cartitem__delete" src={delete1} alt="" />
-        </div>
+      </div>
+      <div
+        className="cartitem__delete__area"
+        onClick={() => deleteProduct(product.id)}
+        style={{ cursor: "pointer" }}
+      >
+        <img className="cartitem__delete" src={delete1} alt="" />
       </div>
     </div>
   )
