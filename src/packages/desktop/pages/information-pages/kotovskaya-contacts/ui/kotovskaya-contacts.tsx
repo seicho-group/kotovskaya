f005a7930b504dfa6f09ec7ffc036ee18dd2624a
Konstantine Fedotovskih <Konstaphy@gmail.com>
2024-11-08 00:49:20 +0500

    убрал все

--- src/packages/desktop/pages/information-pages/kotovskaya-contacts/ui/kotovskaya-contacts.tsx
+++ src/packages/desktop/pages/information-pages/kotovskaya-contacts/ui/kotovskaya-contacts.tsx
@@ -14,20 +14,7 @@ export function KotovskayaContacts() {
       <div className="contacts__wrapper bottom50">
         <div className="h1 bottom25">Контакты</div>
         <div className="contacts__grid">
-          <img src={map} alt="" />
           <div className="left25">
-            <div className="bottom10">Адрес</div>
-            <div className="bottom25">
-              Мы находимся в самом центре Екатеринбурга по адресу: <br /> пр.
-              Ленина, 24/8 (вход через БЦ Ленина 24, сквозной проход)
-            </div>
-            <div className="bottom10">График работы</div>
-            <div className="bottom25">
-              пн: выходной <br />
-              вт-пт: с 11:00 до 19:00 <br />
-              сб: с 11:00 до 18:00 <br />
-              вс: выходной
-            </div>
             <div className="bottom10">Телефон</div>
             <div className="bottom25">{formatPhone(phoneNumber)}</div>
             <div className="bottom10">Почта</div>
